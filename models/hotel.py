2693335745aab745c78fda8b5223537cd6083fb4
Alexsander Junior <alexsander.junior@neoway.com.br>
2020-04-12 14:11:23 -0300

    Add Filters for hotels

--- models/hotel.py
+++ models/hotel.py
@@ -7,19 +7,22 @@ class HotelModel(banco.Model):
     nome = banco.Column(banco.String(80))
     estrelas = banco.Column(banco.Float(precision=1))
     diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
 
-    def __init__(self, hotel_id, nome, estrelas, diaria):
+    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
         self.estrelas = estrelas
         self.diaria = diaria
+        self.cidade = cidade
 
     def json(self):
         return {
             "hotel_id": self.hotel_id,
             "nome": self.nome,
             "estrelas": self.estrelas,
-            "diaria": self.diaria
+            "diaria": self.diaria,
+            "cidade": self.cidade
         }
 
     @classmethod
@@ -30,10 +33,11 @@ class HotelModel(banco.Model):
         banco.session.add(self)
         banco.session.commit()
 
-    def update_hotel(self, nome, estrelas, diaria):
+    def update_hotel(self, nome, estrelas, diaria, cidade):
         self.nome = nome
         self.estrelas = estrelas
         self.diaria = diaria
+        self.cidade = cidade
         self.save_hotel()
 
     def delete_hotel(self):
--- resources/hotel.py
+++ resources/hotel.py
@@ -2,23 +2,83 @@ from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from utility import errors, success, server_code
 from flask_jwt_extended import jwt_required
+import sqlite3
+
 
 class Hoteis(Resource):
+    path_params = reqparse.RequestParser()
+    path_params.add_argument('cidade', type=str)
+    path_params.add_argument('estrelas_min', type=float)
+    path_params.add_argument('estrelas_max', type=float)
+    path_params.add_argument('diaria_min', type=float)
+    path_params.add_argument('diaria_max', type=float)
+    path_params.add_argument('limit', type=float)
+    path_params.add_argument('offset', type=float)
+
     def get(self):
-        return {'hoteis':[hotel.json() for hotel in HotelModel.query.all()]}
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
+
+        dados = self.path_params.parse_args()
+        dados_valids = {chave: valor for chave,
+                        valor in dados.items() if valor is not None}
+        params = self.normalize_path_params(**dados_valids)
+        tupla = tuple([value for value in params.values()])
+        consulta = self.create_sql(**params)
+        result = cursor.execute(consulta, tupla)
+        hoteis = []
+        for hotel in result:
+            hoteis.append({
+                "hotel_id":hotel[0],
+                "nome":hotel[1],
+                "estrelas":hotel[2],
+                "diaria":hotel[3],
+                "cidade":hotel[4],
+            })
+        return {'hoteis':hoteis}, server_code.OK
+
+    def create_sql(self, **kwargs):
+        if kwargs.get('cidade') is None:
+            return "SELECT * FROM hoteis \
+                WHERE (estrelas >= ? AND estrelas <= ?)\
+                    AND (diaria > ? AND diaria <= ?)\
+                        LIMIT ? OFFSET ?"
+        return "SELECT * FROM hoteis \
+                WHERE cidade = ?\
+                    AND (estrelas >= ? AND estrelas <= ?)\
+                    AND (diaria > ? AND diaria <= ?)\
+                        LIMIT ? OFFSET ?"
+
+    def normalize_path_params(self, cidade=None, estrelas_min=0, estrelas_max=5, diaria_min=0, diaria_max=10000, limit=50, offset=0, **kwargs):
+        result = {
+            "cidade": cidade,
+            "estrelas_min": estrelas_min,
+            "estrelas_max": estrelas_max,
+            "diaria_min": diaria_min,
+            "diaria_max": diaria_max,
+            "limit": limit,
+            "offset": offset
+        }
+        if cidade is None:
+            del(result['cidade'])
+
+        return result
+
 
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome', type=str, required=True, help="This field 'nome' canot be null")
+    argumentos.add_argument('nome', type=str, required=True,
+                            help="This field 'nome' canot be null")
     argumentos.add_argument('estrelas', type=float)
     argumentos.add_argument('diaria', type=float)
+    argumentos.add_argument('cidade', type=str)
 
     def get(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel is None:
             return errors._NOT_FOUND
         return hotel.json(), server_code.OK
-        
+
     @jwt_required
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
